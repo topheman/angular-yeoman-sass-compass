9ee1d3bfbb6ddbcbc4ec3b2a2bd04b0e19fc0de5
Christophe Rosset <tophe@topheman.com>
2014-01-04 02:19:43 +0100

    added first-steps.scss + simple todolist management

--- app/scripts/controllers/firstSteps.js
+++ app/scripts/controllers/firstSteps.js
@@ -2,7 +2,20 @@
 
 angular.module('angularYeomanSassCompassApp')
         .controller('FirstStepsCtrl', function($scope) {
-            $scope.showYourNameOrWhatever = function(){
-                window.alert($scope.yourNameOrWhatever !== "" ? $scope.yourNameOrWhatever : "You didn't type anything");
+            $scope.todos = [];
+            $scope.showTodo = function(){
+                window.alert($scope.currentTodo !== "" ? $scope.currentTodo : "You didn't type anything");
+            };
+            $scope.addTodo = function(){
+                if($scope.currentTodo !== ""){
+                    $scope.todos.push($scope.currentTodo);
+                    $scope.currentTodo = "";
+                }
+                else{
+                    window.alert("You didn't type anything");
+                }
+            };
+            $scope.remove = function($index){
+                $scope.todos.splice($index,1);
             };
         });
